2b9ff709995f17e3ec34ca6779b26e80506829ce
Manuel Pereira <manilex2@gmail.com>
2024-03-21 05:55:48 -0500

    Modificado para traer los filtros correctos de contratos

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
-const cors = require("cors");
+// const cors = require("cors");
 const app = express();
 var { expressjwt: jwtExpress } = require("express-jwt");
 const { init, jwt } = require("./helpers/keys")
@@ -16,10 +16,10 @@ app.use(cors());
 app.set('trust proxy', true);
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
-app.use(morgan("dev"));
+// app.use(morgan("dev"));
 app.use(jwtExpress({ secret: JWT_SECRET, algorithms: ['HS256']}).unless({ path: ['/dashboard-server/auth', '/dashboard-server/auth/change-password-reset', '/dashboard-server/auth/reset-password', '/dashboard-server/test', '/dashboard-server'] }));
 
-/* app.use((req, res, next) => {
+app.use((req, res, next) => {
   const allowedOrigins = [`${init.origin_url}/dashboard/`, 'http://localhost:4200'];
   const origin = req.headers.referer;
   
@@ -29,7 +29,7 @@ app.use(jwtExpress({ secret: JWT_SECRET, algorithms: ['HS256']}).unless({ path: 
   } else {
     res.status(403).json({ error: 'Acceso no permitido desde esta ubicación.' });
   }
-}); */
+});
 
 app.get('/dashboard-server', (req, res) => {
     res.send('Hola, Permitida la entrada');
